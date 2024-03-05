24c1e725807300b0349a1b25f8a43b57cdeeaec1
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-03-05 19:51:51 +0100

    Use view model in HomeScreen

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
@@ -2,10 +2,14 @@ package no.uio.ifi.in2000.team18.airborn.ui.home
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun HomeScreen(
-//    viewModel: HomeViewModel = viewModel()
+    viewModel: HomeViewModel = viewModel()
 ) {
+    val state by viewModel.state.collectAsState()
     Text("Welcome")
 }
