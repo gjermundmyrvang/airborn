d9fe2c8afed85a8fa39351a7c1351ddd5a2c95d8
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-03-09 13:41:52 +0100

    add flightbriefscreen mockup

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/FlightBriefScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/FlightBriefScreen.kt
@@ -1,2 +1,86 @@
 package no.uio.ifi.in2000.team18.airborn.ui.flightbrief
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun TestFlightBrief() {
+    FlightBrief()
+}
+
+@Composable
+fun FlightBrief() {
+    val dummyList: List<String> = List(5) { "Bla bla bla bla bla bla bla.  " }
+    LazyColumn(
+        modifier = Modifier
+    ) {
+        items(dummyList) { dummyName ->
+            BriefCard(dummyName = dummyName)
+        }
+    }
+}
+
+@Composable
+fun BriefCard(dummyName: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ), modifier = Modifier
+            .padding(
+                vertical = 4.dp,
+                horizontal = 4.dp
+            )
+    ) {
+        BriefContent(dummyName)
+    }
+}
+
+@Composable
+fun BriefContent(dummyName: String) {
+    var expanded by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .animateContentSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+        ) {
+            Text(text = "TEST")
+            if (expanded) {
+                Text(text = (dummyName).repeat(8))
+            }
+        }
+        IconButton(
+            onClick = { expanded = !expanded }
+        ) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded) "Show less" else "Show more")
+        }
+    }
+}
