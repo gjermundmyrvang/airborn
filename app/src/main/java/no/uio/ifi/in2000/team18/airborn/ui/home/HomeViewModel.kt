69784c38013ae17d770cc85929b81bff794d3fb2
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-03-12 16:27:18 +0100

    Dont access sigchartdatasource in homeviewmodel

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeViewModel.kt
@@ -8,9 +8,7 @@ import no.uio.ifi.in2000.team18.airborn.data.SigchartDataSource
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
-    private val sigchartDataSource: SigchartDataSource // Probably no necessary, but here as an dependency injection example for now
-) : ViewModel() {
+class HomeViewModel @Inject constructor() : ViewModel() {
     data class UiState(
         val dummy: Unit = Unit
     )
