5a626e457bb931ee17ffdae4709588b602469439
henrikun <henrikun@ifi.uio.no>
2024-03-13 13:24:55 +0100

    add homescreen

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
@@ -1,16 +1,17 @@
 package no.uio.ifi.in2000.team18.airborn.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -18,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,15 +30,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     val uistate by viewModel.state.collectAsState()
     val keyboardController = LocalSoftwareKeyboardController.current
-    val coroutineScope = rememberCoroutineScope()
+    var showDropdown by remember { mutableStateOf(false) }
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -46,60 +49,58 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
 
             Spacer(modifier = Modifier.weight(1f))
 
-            OutlinedTextField(
-                value = uistate.airportInput,
-                onValueChange = {
-                    viewModel.filterAirports(it.text)
-                },
-                label = { Text("Start Destination") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardActions = KeyboardActions(onDone = {
-                    keyboardController?.hide()
-                })
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-
-            if (uistate.showDropdown) {
-                DropdownMenu(
-                    expanded = true,
-                    onDismissRequest = {
-                        coroutineScope.launch {
-                            viewModel._state.value =
-                                viewModel._state.value.copy(showDropdown = false)
-                        }
+            ExposedDropdownMenuBox(expanded = showDropdown, onExpandedChange = {
+                showDropdown = it
+            }) {
+                OutlinedTextField(
+                    value = uistate.airportInput,
+                    onValueChange = {
+                        viewModel.filterAirports(it.uppercase(Locale.ROOT))
+                        showDropdown = true
                     },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
+                    singleLine = true,
+                    label = { Text("Start Destination") },
+                    modifier = Modifier.menuAnchor(),
+                    keyboardActions = KeyboardActions(onDone = {
+                        keyboardController?.hide()
+                    })
+                )
+                ExposedDropdownMenu(expanded = showDropdown, onDismissRequest = {
+                    showDropdown = false
+                }) {
                     uistate.airports.forEach { airport ->
                         DropdownMenuItem({ Text(airport) }, onClick = {
-                            coroutineScope.launch {
-                                viewModel.filterAirports(airport)
-                                viewModel._state.value =
-                                    viewModel._state.value.copy(showDropdown = false)
-                            }
-                        })
+                            showDropdown = false
+                            viewModel.selectAirport(airport)
+                            keyboardController?.hide()
+                        },
+                        )
                     }
                 }
             }
-            Spacer(modifier = Modifier.weight(2f))
-            Button(onClick = {},
+            Spacer(modifier = Modifier.weight(3f))
+            Button(
+                onClick = {},
                 modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 32.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Green)){
+                    .fillMaxWidth()
+                    .padding(bottom = 32.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
+            ) {
                 Text("Generer flightbrief", fontSize = 18.sp)
             }
         }
     }
 }
+
 @Preview
 @Composable
-fun HomeScreenPreview(){
-    val navController = rememberNavController()
-    val mockViewModel = HomeViewModel().apply { filterAirports("")}
+fun HomeScreenPreview() {
+    val mockViewModel = HomeViewModel().apply { filterAirports("") }
     HomeScreen(viewModel = mockViewModel)
 }
--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeViewModel.kt
@@ -4,15 +4,34 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import no.uio.ifi.in2000.team18.airborn.data.SigchartDataSource
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
     data class UiState(
-        val dummy: Unit = Unit
+        val airportInput: String = "",
+        val airports: List<String> = listOf("ENGM", "ENSB", "ENBR", "ENVA", "ENGB", "ENGC", "ENGD", "ENGE", "ENGF"),
     )
 
     private val _state = MutableStateFlow(UiState())
     val state = _state.asStateFlow()
+
+    fun filterAirports(input: String) {
+        _state.update {
+            it.copy(
+                airportInput = input,
+                airports = _state.value.airports.filter { airport ->
+                    airport.startsWith(input, ignoreCase = true)
+                },
+            )
+        }
+    }
+
+    fun selectAirport(airport: String) {
+        _state.update {
+            it.copy(airportInput = airport)
+        }
+
+    }
 }
