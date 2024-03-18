131b3f7ec3b1fd0f9dc7d607ae0addd2fc4d175c
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-03-18 12:49:12 +0100

    Generate Flightbrief Id

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/FlightbriefRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/FlightbriefRepository.kt
@@ -7,6 +7,7 @@ import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Icao
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.MetarTaf
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Position
 import java.time.LocalDateTime
+import java.util.UUID
 
 class FlightbriefRepository constructor(
     val sigchartDataSource: SigchartDataSource,
@@ -22,13 +23,17 @@ class FlightbriefRepository constructor(
     /**
      * @param to if this is null it means that it should be the same as from
      */
-    suspend fun createFlightbrief(from: Icao, to: Icao?, time: LocalDateTime) {
+    suspend fun createFlightbrief(from: Icao, to: Icao?, time: LocalDateTime): String {
         val brief = Flightbrief(
             departure = createAirportBrief(from, time),
             arrival = to?.let { createAirportBrief(it, time) }, // TODO: calculate arrival time
             altArrivals = listOf(),
             sigchart = sigchartDataSource.findSigchart(time)
         )
+
+        val id = UUID.randomUUID().toString() // This is always unique. There are more uuids than atoms in the observable universe
+        flightbriefs[id] = brief
+        return id
     }
 
     private suspend fun createAirportBrief(icao: Icao, time: LocalDateTime): AirportBrief =
