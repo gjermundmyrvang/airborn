9e2f41433e293d5c39f50173ce75c99f5b36c9f0
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-03-21 13:34:51 +0100

    add LocationForecastDataSource, repository and relevant models for fetching locationforecast data

---
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/LocationForecastRepository.kt
@@ -0,0 +1,8 @@
+package no.uio.ifi.in2000.team18.airborn.data
+
+import javax.inject.Inject
+
+class LocationForecastRepository(private val locationForecastDataSource: LocationForecastDataSource) { // Will eventually use Dagger&Hilt
+    
+
+}
