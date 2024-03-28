9855615157d26a66f22f595b3ae985eb96995eaf
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-03-28 20:49:49 +0100

    remove old HomeScreen composable and unneccesary linebreaks

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/HomeScreen.kt
@@ -39,127 +39,6 @@ import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.team18.airborn.LocalNavController
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Airport
 
-/*
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
-    val navController = LocalNavController.current
-    val uistate by viewModel.state.collectAsState()
-    val keyboardController = LocalSoftwareKeyboardController.current
-    var showDepartureDropdown by remember { mutableStateOf(false) }
-    var showArrivalDropdown by remember { mutableStateOf(false) }
-
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-
-            Spacer(modifier = Modifier.height(80.dp))
-            Text(text = "Departure from:")
-            ExposedDropdownMenuBox(expanded = showDepartureDropdown, onExpandedChange = {
-                showDepartureDropdown = it
-            }) {
-                OutlinedTextField(
-                    value = uistate.departureAirportInput,
-                    onValueChange = {
-                        viewModel.filterDepartureAirports(it)
-                        showDepartureDropdown = true
-                    },
-                    singleLine = true,
-                    label = { Text("Start Destination") },
-                    modifier = Modifier.menuAnchor(),
-                    keyboardActions = KeyboardActions(onDone = {
-                        keyboardController?.hide()
-                    })
-                )
-                ExposedDropdownMenu(expanded = showDepartureDropdown, onDismissRequest = {
-                    showDepartureDropdown = false
-                }) {
-                    uistate.departureAirports.forEach { airport ->
-                        DropdownMenuItem(
-                            {
-                                Column {
-                                    Text(airport.icao.code)
-                                    Text(airport.name)
-                                }
-                            },
-                            onClick = {
-                                showDepartureDropdown = false
-                                viewModel.selectDepartureAirport(airport.icao.code)
-                                keyboardController?.hide()
-                            },
-                        )
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(80.dp))
-            Text(text = "Arriving at:")
-            ExposedDropdownMenuBox(expanded = showArrivalDropdown, onExpandedChange = {
-                showArrivalDropdown = it
-            }) {
-                OutlinedTextField(
-                    value = uistate.arrivalAirportInput,
-                    onValueChange = {
-                        viewModel.filterArrivalAirports(it)
-                        showArrivalDropdown = true
-                    },
-                    singleLine = true,
-                    label = { Text("End Destination") },
-                    modifier = Modifier.menuAnchor(),
-                    keyboardActions = KeyboardActions(onDone = {
-                        keyboardController?.hide()
-                    }),
-                )
-                ExposedDropdownMenu(expanded = showArrivalDropdown, onDismissRequest = {
-                    showArrivalDropdown = false
-                }) {
-                    uistate.arrivalAirports.forEach { airport ->
-                        DropdownMenuItem(
-                            {
-                                Column {
-                                    Text(airport.icao.code)
-                                    Text(airport.name)
-                                }
-                            },
-                            onClick = {
-                                showArrivalDropdown = false
-                                viewModel.selectArrivalAirport(airport.icao.code)
-                                keyboardController?.hide()
-                            },
-                        )
-                    }
-                }
-            }
-            val scope = rememberCoroutineScope()
-            Spacer(modifier = Modifier.weight(3f))
-            Button(
-                onClick = {
-                    // Generate flightbrief
-                    scope.launch {
-                        val id = viewModel.generateFlightbrief()
-                        navController.navigate("flightbrief/$id")
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 32.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xff62c4c3))
-            ) {
-                Text("Generer flightbrief", fontSize = 18.sp)
-            }
-        }
-    }
-}
- */
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen2(
@@ -193,21 +72,17 @@ fun HomeScreen2(
                         keyboardController?.hide()
                     }),
                 )
-                LazyColumn(
-                    modifier = modifier
-                        .imePadding(),
-                    content = {
-                        items(airports) { airport ->
-                            AirportInfoRow(item = airport) { clickedAirport ->
-                                viewModel.selectAirport(clickedAirport.icao.code)
-                                scope.launch {
-                                    val id = viewModel.generateFlightbrief()
-                                    navController.navigate("flightbrief/$id")
-                                }
+                LazyColumn(modifier = modifier.imePadding(), content = {
+                    items(airports) { airport ->
+                        AirportInfoRow(item = airport) { clickedAirport ->
+                            viewModel.selectAirport(clickedAirport.icao.code)
+                            scope.launch {
+                                val id = viewModel.generateFlightbrief()
+                                navController.navigate("flightbrief/$id")
                             }
                         }
                     }
-                )
+                })
             }
         }, sheetPeekHeight = 300.dp, content = {
             Column(
