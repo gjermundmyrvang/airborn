58c7cd27255b7da36117c264af5bcb585811da03
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-03-29 12:46:30 +0100

    mention windy for using their webcam API

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/WebcamScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/WebcamScreen.kt
@@ -3,15 +3,18 @@ package no.uio.ifi.in2000.team18.airborn.ui.webcam
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -22,86 +25,148 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import no.uio.ifi.in2000.team18.airborn.model.Webcam
-import no.uio.ifi.in2000.team18.airborn.ui.common.LoadingState
-import no.uio.ifi.in2000.team18.airborn.ui.flightbrief.LoadingCollapsible
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WebcamSection(state: LoadingState<List<Webcam>>) =
-    LoadingCollapsible(state, header = "Webcams") { webcams ->
-        var expanded by remember { mutableStateOf(false) }
-        if (webcams.isEmpty()) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "No webcams available in 20km radius", fontSize = 30.sp
-                )
-            }
-        } else {
-            var currentWebcam by remember { mutableStateOf(webcams.first()) }
-            Column(
+fun WebcamSection(webcams: List<Webcam>) {
+    var expanded by remember { mutableStateOf(false) }
+    if (webcams.isEmpty()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "No webcams available in 20km radius", fontSize = 30.sp
+            )
+        }
+    } else {
+        var currentWebcam by remember { mutableStateOf(webcams.first()) }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            SubcomposeAsyncImage(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth,
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(currentWebcam.images.current.preview).build(),
+                loading = {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(30.dp),
+                            color = MaterialTheme.colorScheme.onBackground,
+                            strokeWidth = 1.dp
+                        )
+                    }
+                },
+                contentDescription = "Webcam image"
+            )
+            Text(text = "last updated: ${currentWebcam.lastUpdatedOn}")
+            HyperlinkText(
+                fullText = "Webcams provided by windy.com — add a webcam",
+                linkText = listOf("windy.com", "add a webcam"),
+                hyperlinks = listOf("https://www.windy.com/", "https://www.windy.com/webcams/add")
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {
                     expanded = it
+                },
+            ) {
+                OutlinedTextField(
+                    value = currentWebcam.title,
+                    onValueChange = {
+                        expanded = true
+                    },
+                    readOnly = true,
+                    label = { Text("Change webcam") },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(expanded = expanded, onDismissRequest = {
+                    expanded = false
                 }) {
-                    OutlinedTextField(
-                        value = currentWebcam.title,
-                        onValueChange = {
-                            expanded = true
-                        },
-                        readOnly = true,
-                        label = { Text("Change webcam") },
-                        modifier = Modifier.menuAnchor()
-                    )
-                    ExposedDropdownMenu(expanded = expanded, onDismissRequest = {
-                        expanded = false
-                    }) {
-                        webcams.forEach { webcam ->
-                            DropdownMenuItem(
-                                {
-                                    Text(webcam.title)
-                                },
-                                onClick = {
-                                    currentWebcam = webcam
-                                    expanded = false
-                                },
-                            )
-                        }
+                    webcams.forEach { webcam ->
+                        DropdownMenuItem(
+                            {
+                                Text(webcam.title)
+                            },
+                            onClick = {
+                                currentWebcam = webcam
+                                expanded = false
+                            },
+                        )
+                        HorizontalDivider(
+                            thickness = 1.dp, color = MaterialTheme.colorScheme.outline
+                        )
                     }
                 }
-                Spacer(modifier = Modifier.height(16.dp))
-                SubcomposeAsyncImage(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentScale = ContentScale.FillWidth,
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(currentWebcam.images.current.preview).build(),
-                    loading = {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            CircularProgressIndicator(
-                                modifier = Modifier.size(30.dp),
-                                color = MaterialTheme.colorScheme.onBackground,
-                                strokeWidth = 1.dp
-                            )
-                        }
-                    },
-                    contentDescription = "Webcam image"
-                )
             }
         }
     }
+}
+
+@Composable
+fun HyperlinkText(
+    modifier: Modifier = Modifier,
+    fullText: String,
+    linkText: List<String>,
+    linkTextColor: Color = Color.Blue,
+    linkTextFontWeight: FontWeight = FontWeight.Medium,
+    linkTextDecoration: TextDecoration = TextDecoration.Underline,
+    hyperlinks: List<String>,
+    fontSize: TextUnit = TextUnit.Unspecified
+) {
+    val annotatedString = buildAnnotatedString {
+        append(fullText)
+        linkText.forEachIndexed { index, link ->
+            val startIndex = fullText.indexOf(link)
+            val endIndex = startIndex + link.length
+            addStyle(
+                style = SpanStyle(
+                    color = linkTextColor,
+                    fontSize = fontSize,
+                    fontWeight = linkTextFontWeight,
+                    textDecoration = linkTextDecoration
+                ), start = startIndex, end = endIndex
+            )
+            addStringAnnotation(
+                tag = "URL", annotation = hyperlinks[index], start = startIndex, end = endIndex
+            )
+        }
+        addStyle(
+            style = SpanStyle(
+                fontSize = fontSize
+            ), start = 0, end = fullText.length
+        )
+    }
+
+    val uriHandler = LocalUriHandler.current
+
+    ClickableText(modifier = modifier, text = annotatedString, onClick = {
+        annotatedString.getStringAnnotations("URL", it, it).firstOrNull()?.let { stringAnnotation ->
+            uriHandler.openUri(stringAnnotation.item)
+        }
+    })
+}
