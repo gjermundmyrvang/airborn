963960a41033f47b7989fad2cf4c978242bbd5cc
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-01 22:51:02 +0200

    change currentWebcam variable from rememberSaveable to just remember because saveable causes app to crash

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/WebcamScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/WebcamScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,7 +55,7 @@ fun WebcamSection(webcams: List<Webcam>) {
             )
         }
     } else {
-        var currentWebcam by rememberSaveable { mutableStateOf(webcams.first()) }
+        var currentWebcam by remember { mutableStateOf(webcams.first()) }
         Column(
             modifier = Modifier
                 .fillMaxSize()
