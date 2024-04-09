d1ce4bf93c8a4d26aee5b503cb7284017e3590fa
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-04-09 17:44:55 +0200

    Add basic error message composable

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/Components.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/Components.kt
@@ -3,8 +3,11 @@ package no.uio.ifi.in2000.team18.airborn.ui.flightbrief
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -23,6 +27,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,10 +38,41 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import no.uio.ifi.in2000.team18.airborn.ui.common.LoadingState
+
+@Composable
+fun Error(
+    message: String, modifier: Modifier = Modifier
+) = Box(modifier = modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                BorderStroke(width = 2.dp, color = Color.Red),
+                shape = RoundedCornerShape(6.dp),
+            )
+            .padding(8.dp)
+    ) {
+        Text(text = "ERROR")
+        Text(text = message)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewError() {
+    Surface {
+        Box(modifier = Modifier
+            .padding(16.dp)
+            .width(500.dp)) {
+            Error("Failed to load Metar/Taf: Unresolved Address")
+        }
+    }
+}
 
 @Composable
 fun MultiToggleButton(
@@ -102,7 +138,7 @@ fun <T> LoadingCollapsible(
             CircularProgressIndicator(modifier = Modifier.padding(start = 16.dp))
         }
 
-        is LoadingState.Error -> Text("ERROR")
+        is LoadingState.Error -> Error("failed to load ${header}: ${value.message}", modifier = Modifier.padding(16.dp, 8.dp))
     }
 }
 
