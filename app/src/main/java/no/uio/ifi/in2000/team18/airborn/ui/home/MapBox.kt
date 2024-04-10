da2f02ca361d53512d18a5ba46759f67494c5c7a
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-10 16:03:30 +0200

    add red marker image and add composable for displaying all airports on the map with the red marker annotation

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/MapBox.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/MapBox.kt
@@ -1,2 +1,90 @@
 package no.uio.ifi.in2000.team18.airborn.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import com.mapbox.maps.extension.compose.annotation.ViewAnnotation
+import com.mapbox.maps.viewannotation.geometry
+import com.mapbox.maps.viewannotation.viewAnnotationOptions
+import no.uio.ifi.in2000.team18.airborn.R
+
+@OptIn(MapboxExperimental::class)
+@Composable
+fun MapBoxHomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) =
+    Column(modifier = Modifier.fillMaxSize()) {
+        val state by homeViewModel.state.collectAsState()
+        val airports = state.airports
+        val startPos = airports.first().position
+
+        MapboxMap(
+            Modifier.fillMaxSize(),
+            mapViewportState = MapViewportState().apply {
+                setCameraOptions {
+                    zoom(13.0)
+                    center(Point.fromLngLat(startPos.longitude, startPos.latitude))
+                    pitch(0.0)
+                    bearing(0.0)
+                }
+            },
+        ) {
+            airports.forEach { airport ->
+                Annotation(lon = airport.position.longitude, lat = airport.position.latitude)
+            }
+        }
+    }
+
+
+@OptIn(MapboxExperimental::class)
+@Composable
+fun Annotation(lon: Double, lat: Double) {
+    val selfLocationPoint: Point by remember {
+        mutableStateOf(
+            Point.fromLngLat(
+                lon, lat
+            )
+        )
+    }
+    ViewAnnotation(
+        options = viewAnnotationOptions {
+            geometry(selfLocationPoint)
+            allowOverlap(true)
+        },
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.red_marker),
+            contentDescription = "Marker",
+            modifier = Modifier.size(20.dp)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewAnnotation() = Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+) {
+    Image(
+        painter = painterResource(id = R.drawable.red_marker),
+        contentDescription = "Marker",
+        modifier = Modifier.size(20.dp)
+    )
+}
