0c383798537b5f1ba6e8fb0456b1e3d35fc6c39a
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-04-15 12:42:53 +0200

    Merge di modules to one modules

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/di/AppModule.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/di/AppModule.kt
@@ -1,8 +1,11 @@
 package no.uio.ifi.in2000.team18.airborn.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
@@ -10,11 +13,13 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.header
 import io.ktor.serialization.gson.gson
+import no.uio.ifi.in2000.team18.airborn.data.dao.BuiltinAirportDao
+import no.uio.ifi.in2000.team18.airborn.data.datasource.AppDatabase
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class HttpClient {
+class AppModule {
     @Provides
     @Singleton
     fun provideKtorClient(): HttpClient = HttpClient(CIO) {
@@ -26,4 +31,18 @@ class HttpClient {
             gson()
         }
     }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "airborn.db"
+        ).createFromAsset("database/airborn.db").build()
+
+    @Provides
+    @Singleton
+    fun provideBuiltinAirportDao(database: AppDatabase): BuiltinAirportDao =
+        database.builtinAirportDao
 }
