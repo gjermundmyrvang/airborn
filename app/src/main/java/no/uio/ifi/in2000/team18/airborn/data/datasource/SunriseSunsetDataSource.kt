f126f87206fbb655148379238668364d9d1dad3a
Ola Nordrum Isachsen <olanis@uio.no>
2024-04-15 15:52:42 +0200

    Add SunriseSunset model

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/datasource/SunriseSunsetDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/datasource/SunriseSunsetDataSource.kt
@@ -3,10 +3,11 @@ package no.uio.ifi.in2000.team18.airborn.data.datasource
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import no.uio.ifi.in2000.team18.airborn.model.SunriseSunset
 import javax.inject.Inject
 
 class SunriseSunsetDataSource @Inject constructor(val client: HttpClient) {
-    suspend fun fetchSunriseSunset(lat: Int, lon: Int) {
+    suspend fun fetchSunriseSunset(lat: Int, lon: Int): List<SunriseSunset> {
         return client.get("weatherapi/sunrise/3.0/sun?lat=$lat&lon=$lon")
             .body()
     }
