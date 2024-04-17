75bc13ee6b3bebe63bd24bcf8e84ef0ed9feb79e
Ola Nordrum Isachsen <olanis@uio.no>
2024-04-17 17:06:13 +0200

    Add get sunrise function and map for Sun objects in airport repo

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/AirportRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/AirportRepository.kt
@@ -2,6 +2,7 @@ package no.uio.ifi.in2000.team18.airborn.data.repository
 
 import no.uio.ifi.in2000.team18.airborn.data.datasource.AirportDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.SigchartDataSource
+import no.uio.ifi.in2000.team18.airborn.data.datasource.SunriseSunsetDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.TafmetarDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.TurbulenceDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.WebcamDataSource
@@ -12,8 +13,12 @@ import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Airport
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Icao
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Metar
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.MetarTaf
+import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Sun
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Taf
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.TurbulenceMapAndCross
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 class AirportRepository @Inject constructor(
@@ -22,11 +27,15 @@ class AirportRepository @Inject constructor(
     private val sigchartDataSource: SigchartDataSource,
     private val turbulenceDataSource: TurbulenceDataSource,
     private val webcamDataSource: WebcamDataSource,
+    private val sunriseSunsetDataSource: SunriseSunsetDataSource
 ) {
     // Airport logic
     suspend fun getByIcao(icao: Icao): Airport? = airportDataSource.getByIcao(icao)
     suspend fun search(query: String): List<Airport> = airportDataSource.search(query)
     suspend fun all(): List<Airport> = airportDataSource.all()
+
+    private var sunMap = HashMap<Icao, Sun>()
+
 
     // TAF/METAR logic
     suspend fun fetchTafMetar(icao: Icao): MetarTaf {
@@ -57,4 +66,33 @@ class AirportRepository @Inject constructor(
 
     suspend fun fetchWebcamImages(airport: Airport): List<Webcam> =
         webcamDataSource.fetchImage(airport)
+
+
+    // Sunrise & Sunset logic
+    suspend fun getSunriseSunset(airport: Airport): Sun {
+
+        val sunData = sunMap[airport.icao]
+
+        return if (sunData != null) {
+            sunData
+        } else {
+
+            val sun = sunriseSunsetDataSource.fetchSunriseSunset(
+                airport.position.latitude, airport.position.longitude
+            )
+
+            val formatter = DateTimeFormatter.ofPattern("kk:mm")
+
+            val sunrise = ZonedDateTime.parse(sun.properties.sunrise.time)
+                .withZoneSameInstant(ZoneId.systemDefault()).format(formatter)
+
+            val sunset = ZonedDateTime.parse(sun.properties.sunset.time)
+                .withZoneSameInstant(ZoneId.systemDefault()).format(formatter)
+
+            val newSun = Sun(sunrise, sunset)
+
+            sunMap[airport.icao] = newSun
+            newSun
+        }
+    }
 }
