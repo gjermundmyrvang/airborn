ad05b3b94a69f6bcae414f2ee3bb539ae365b4e9
Falk Markus Dursun Jetlund <fmjetlun@uio.no>
2024-04-17 10:59:29 +0200

    Add network unavailable animation

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/MainActivity.kt
@@ -3,6 +3,9 @@ package no.uio.ifi.in2000.team18.airborn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
@@ -43,21 +47,34 @@ class MainActivity @Inject constructor() : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        if (networkStatus != ConnectivityObserver.Status.Available) {
-                            Box(
-                                modifier = Modifier
-                                    .background(Color.Red)
-                                    .padding(16.dp)
-                                    .fillMaxWidth()
-                            ) {
-                                Text("$networkStatus")
-                            }
-                        }
+                    NetworkStatus(status = networkStatus) {
                         Navigation()
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun NetworkStatus(status: ConnectivityObserver.Status, content: @Composable () -> Unit) = Column {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Red)
+            .animateContentSize(
+                animationSpec = tween(durationMillis = 300, easing = LinearOutSlowInEasing)
+            )
+    ) {
+        if (status != ConnectivityObserver.Status.Available) {
+            Box(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Network connection ${status.toString().lowercase()}")
+            }
+        }
+    }
+    content()
 }
