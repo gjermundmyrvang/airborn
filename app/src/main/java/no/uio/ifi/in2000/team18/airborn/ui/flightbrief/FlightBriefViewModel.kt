500c10f9e255ddb37a8b239cf83adfe7c81843f2
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-18 15:05:35 +0200

    add state hasArrival in flightbriefviewmodel and state for departureicao to navigate correctly when adding arrival lazy

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/FlightBriefViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/FlightBriefViewModel.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.team18.airborn.ui.flightbrief
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,6 +11,8 @@ import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.team18.airborn.data.repository.AirportRepository
 import no.uio.ifi.in2000.team18.airborn.model.Area
 import no.uio.ifi.in2000.team18.airborn.model.Sigchart
+import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Airport
+import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Icao
 import no.uio.ifi.in2000.team18.airborn.ui.common.LoadingState
 import no.uio.ifi.in2000.team18.airborn.ui.common.LoadingState.Loading
 import no.uio.ifi.in2000.team18.airborn.ui.common.toSuccess
@@ -18,20 +21,47 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FlightBriefViewModel @Inject constructor(
-    private val airportRepository: AirportRepository,
+    private val airportRepository: AirportRepository, savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     data class UiState(
         val sigcharts: LoadingState<Map<Area, List<Sigchart>>> = Loading,
+        val hasArrival: Boolean,
+        val arrivalAirportInput: String = "",
+        val airports: List<Airport> = listOf(),
+        val departureIcao: Icao,
     )
 
-    private val _state = MutableStateFlow(UiState())
+    private val _state = MutableStateFlow(
+        UiState(
+            hasArrival = savedStateHandle.get<String>("arrivalIcao") != "null",
+            departureIcao = Icao(savedStateHandle.get<String>("departureIcao")!!)
+        )
+    )
     val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    airports = airportRepository.all()
+                )
+            }
+        }
+    }
 
     fun initSigchart() {
         viewModelScope.launch {
             val sigcharts = load { airportRepository.getSigcharts() }
             _state.update { it.copy(sigcharts = sigcharts) }
+        }
+    }
+
+    fun filterArrivalAirports(input: String) {
+        _state.update { it.copy(arrivalAirportInput = input) }
+        viewModelScope.launch {
+            val result = airportRepository.search(input)
+            _state.update { it.copy(airports = result) }
         }
     }
 
