0886c1570e7ed522bc166a459bf35dec034c6c42
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-21 13:27:24 +0200

    add offshoremaps datasource and add to airportrepository

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/AirportRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/AirportRepository.kt
@@ -2,12 +2,14 @@ package no.uio.ifi.in2000.team18.airborn.data.repository
 
 import no.uio.ifi.in2000.team18.airborn.data.datasource.AirportDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.GeosatelliteDataSource
+import no.uio.ifi.in2000.team18.airborn.data.datasource.OffshoreMapsDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.SigchartDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.SunriseSunsetDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.TafmetarDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.TurbulenceDataSource
 import no.uio.ifi.in2000.team18.airborn.data.datasource.WebcamDataSource
 import no.uio.ifi.in2000.team18.airborn.model.Area
+import no.uio.ifi.in2000.team18.airborn.model.OffshoreMap
 import no.uio.ifi.in2000.team18.airborn.model.Sigchart
 import no.uio.ifi.in2000.team18.airborn.model.Webcam
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Airport
@@ -28,7 +30,8 @@ class AirportRepository @Inject constructor(
     private val sigchartDataSource: SigchartDataSource,
     private val turbulenceDataSource: TurbulenceDataSource,
     private val webcamDataSource: WebcamDataSource,
-    private val sunriseSunsetDataSource: SunriseSunsetDataSource
+    private val sunriseSunsetDataSource: SunriseSunsetDataSource,
+    private val offshoreMapsDataSource: OffshoreMapsDataSource,
     private val geosatelliteDataSource: GeosatelliteDataSource,
 ) {
     // Airport logic
@@ -97,5 +100,9 @@ class AirportRepository @Inject constructor(
             newSun
         }
     }
+
+    suspend fun getOffshoreMaps(): Map<String, List<OffshoreMap>> =
+        offshoreMapsDataSource.fetchOffshoreMaps().groupBy { it.endpoint }
+
     fun getGeosatelliteImage(): String = geosatelliteDataSource.fetchGeosatelliteImage()
 }
