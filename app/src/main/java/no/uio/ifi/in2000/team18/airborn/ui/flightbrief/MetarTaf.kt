8a1b1f06c2aec014336d2a2f5e9fa9f9d6767231
otberger <otberger@uio.no>
2024-04-23 13:32:06 +0200

    New icons

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/MetarTaf.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/MetarTaf.kt
@@ -1,7 +1,6 @@
 package no.uio.ifi.in2000.team18.airborn.ui.flightbrief
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -23,17 +20,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format
 import kotlinx.datetime.format.FormatStringsInDatetimeFormats
 import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.toKotlinTimeZone
 import kotlinx.datetime.toLocalDateTime
+import no.uio.ifi.in2000.team18.airborn.data.repository.parsers.parseMetar
 import no.uio.ifi.in2000.team18.airborn.model.Direction
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Cav
 import no.uio.ifi.in2000.team18.airborn.model.flightbrief.CloudType
@@ -47,7 +54,6 @@ import no.uio.ifi.in2000.team18.airborn.ui.common.LoadingState
 import no.uio.ifi.in2000.team18.airborn.ui.common.RotatableArrowIcon
 import java.time.ZoneId
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MetarTaf(state: LoadingState<MetarTaf?>, initMetar: () -> Unit) =
     LazyCollapsible(header = "Metar/Taf", value = state, onExpand = initMetar) { metarTaf ->
@@ -85,11 +91,13 @@ fun MetarTaf(state: LoadingState<MetarTaf?>, initMetar: () -> Unit) =
                 ) {
                     Row {
                         Text(
-                            text = "RAW:",
+                            text = "RAW DATA:",
                             fontWeight = FontWeight.Bold,
-                            modifier = Modifier.weight(1F)
+                            modifier = Modifier
+                                .weight(1F)
+                                .fillMaxSize()
                         )
-                        Icon(imageVector = Icons.Outlined.Info, contentDescription = "Flip")
+                        Icon(imageVector = rememberFlip(), contentDescription = "Flip")
                     }
                     if (metar != null) {
                         Text(
@@ -140,9 +148,11 @@ fun DecodedMetar(metar: Metar) = Column(
         Text(
             text = "DECODED",
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.weight(1F)
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxSize()
         )
-        Icon(imageVector = Icons.Outlined.Info, contentDescription = "Flip")
+        Icon(imageVector = rememberFlip(), contentDescription = "Flip")
     }
     Text(
         text = "METAR",
@@ -256,12 +266,16 @@ fun MetarClouds(clouds: Clouds) {
             clouds.layers.forEach {
                 Row {
                     Text("Cloud Layer: ", fontWeight = FontWeight.Bold)
-                    if (it.type == CloudType.Nothing) {
-                        Text("${it.cover} clouds at ${it.height.formatAsFeet()}") // TODO: Make more human readable
-                    } else if (it.type == CloudType.Unknown) {
-                        Text("${it.cover} clouds of unknown type at ${it.height}")
-                    } else {
-                        Text("${it.cover} ${it.type} at ${it.height}")
+                    when (it.type) {
+                        CloudType.Nothing -> {
+                            Text("${it.cover} clouds at ${it.height.formatAsFeet()}") // TODO: Make more human readable
+                        }
+                        CloudType.Unknown -> {
+                            Text("${it.cover} clouds of unknown type at ${it.height}")
+                        }
+                        else -> {
+                            Text("${it.cover} ${it.type} at ${it.height}")
+                        }
                     }
                 }
             }
@@ -271,28 +285,118 @@ fun MetarClouds(clouds: Clouds) {
         Clouds.NSC -> Text("No significant clouds", fontWeight = FontWeight.Bold)
     }
 }
-/*
+
+@Composable
+fun rememberFlip(): ImageVector {
+    return remember {
+        ImageVector.Builder(
+            name = "flip",
+            defaultWidth = 40.0.dp,
+            defaultHeight = 40.0.dp,
+            viewportWidth = 40.0f,
+            viewportHeight = 40.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black),
+                fillAlpha = 1f,
+                stroke = null,
+                strokeAlpha = 1f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(7.875f, 34.75f)
+                quadToRelative(-1.042f, 0f, -1.833f, -0.792f)
+                quadToRelative(-0.792f, -0.791f, -0.792f, -1.833f)
+                verticalLineTo(7.875f)
+                quadToRelative(0f, -1.042f, 0.792f, -1.833f)
+                quadToRelative(0.791f, -0.792f, 1.833f, -0.792f)
+                horizontalLineToRelative(6.917f)
+                quadToRelative(0.541f, 0f, 0.937f, 0.396f)
+                reflectiveQuadToRelative(0.396f, 0.937f)
+                quadToRelative(0f, 0.542f, -0.396f, 0.917f)
+                reflectiveQuadToRelative(-0.937f, 0.375f)
+                horizontalLineTo(7.875f)
+                verticalLineToRelative(24.25f)
+                horizontalLineToRelative(6.917f)
+                quadToRelative(0.541f, 0f, 0.937f, 0.375f)
+                reflectiveQuadToRelative(0.396f, 0.917f)
+                quadToRelative(0f, 0.583f, -0.396f, 0.958f)
+                reflectiveQuadToRelative(-0.937f, 0.375f)
+                close()
+                moveToRelative(12.208f, 3.417f)
+                quadToRelative(-0.583f, 0f, -0.958f, -0.375f)
+                reflectiveQuadToRelative(-0.375f, -0.959f)
+                verticalLineTo(3.208f)
+                quadToRelative(0f, -0.541f, 0.396f, -0.937f)
+                reflectiveQuadToRelative(0.937f, -0.396f)
+                quadToRelative(0.542f, 0f, 0.917f, 0.396f)
+                reflectiveQuadToRelative(0.375f, 0.937f)
+                verticalLineToRelative(33.625f)
+                quadToRelative(0f, 0.584f, -0.375f, 0.959f)
+                reflectiveQuadToRelative(-0.917f, 0.375f)
+                close()
+                moveTo(32.125f, 7.875f)
+                horizontalLineToRelative(-0.417f)
+                verticalLineTo(5.25f)
+                horizontalLineToRelative(0.417f)
+                quadToRelative(1.042f, 0f, 1.833f, 0.792f)
+                quadToRelative(0.792f, 0.791f, 0.792f, 1.833f)
+                verticalLineToRelative(0.417f)
+                horizontalLineToRelative(-2.625f)
+                close()
+                moveToRelative(0f, 14.25f)
+                verticalLineToRelative(-4.25f)
+                horizontalLineToRelative(2.625f)
+                verticalLineToRelative(4.25f)
+                close()
+                moveToRelative(0f, 12.625f)
+                horizontalLineToRelative(-0.417f)
+                verticalLineToRelative(-2.625f)
+                horizontalLineToRelative(0.417f)
+                verticalLineToRelative(-0.458f)
+                horizontalLineToRelative(2.625f)
+                verticalLineToRelative(0.458f)
+                quadToRelative(0f, 1.042f, -0.792f, 1.833f)
+                quadToRelative(-0.791f, 0.792f, -1.833f, 0.792f)
+                close()
+                moveToRelative(0f, -19.5f)
+                verticalLineToRelative(-4.292f)
+                horizontalLineToRelative(2.625f)
+                verticalLineToRelative(4.292f)
+                close()
+                moveToRelative(0f, 13.792f)
+                verticalLineTo(24.75f)
+                horizontalLineToRelative(2.625f)
+                verticalLineToRelative(4.292f)
+                close()
+                moveToRelative(-8.083f, 5.708f)
+                verticalLineToRelative(-2.625f)
+                horizontalLineToRelative(5f)
+                verticalLineToRelative(2.625f)
+                close()
+                moveToRelative(0f, -26.875f)
+                verticalLineTo(5.25f)
+                horizontalLineToRelative(5f)
+                verticalLineToRelative(2.625f)
+                close()
+            }
+        }.build()
+    }
+}
 @Preview(showSystemUi = true)
 @Composable
 fun TestMetarDecode() = Column {
-    var rotated by remember {
-        mutableStateOf(false)
-    }
-    val rotate by animateFloatAsState(
-            targetValue = if (rotated) 180f else 0f,
-            animationSpec = tween(500)
-        )
     DecodedMetar(
         parseMetar(
             "ENSG 150720Z 07207KT 030V100 9999 400N R33/P2000 FEW040 SCT090 BKN100TCU 02/M02 Q1001 RMK WIND 3806FT 10015KT=",
             Instant.parse("2024-04-16T00:00:00Z")
-        ).expect(),
-        rotate
+        ).expect()
     )
     DecodedMetar(
         parseMetar(
-            "ENSS 152150Z 07014KT 1100 R33/P2000 -SN VCSHRAFGSS -VCSHSNFG -VCFG +FGSQ +SQ +VCTSRASNSQ SCT005 BKN015 OVC038 M01/M02 Q1009 RMK WIND 0500FT VRB04KT=").expect(),
-        rotate
+            "ENSS 152150Z 07014KT 1100 R33/P2000 -SN VCSHRAFGSS -VCSHSNFG -VCFG +FGSQ +SQ +VCTSRASNSQ SCT005 BKN015 OVC038 M01/M02 Q1009 RMK WIND 0500FT VRB04KT=").expect()
         )
 }
-*/
