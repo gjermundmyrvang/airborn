f42fc3c79c9b66e00b58583ede34347fa8becf65
Ola Nordrum Isachsen <olanis@uio.no>
2024-04-24 22:11:36 +0200

    Add RouteIsobaric dataclass

---
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/model/RouteIsobaric.kt
@@ -0,0 +1,12 @@
+package no.uio.ifi.in2000.team18.airborn.model
+
+import no.uio.ifi.in2000.team18.airborn.model.flightbrief.Airport
+import no.uio.ifi.in2000.team18.airborn.model.isobaric.IsobaricData
+
+data class RouteIsobaric(
+    val departure: Airport,
+    val arrival: Airport,
+    val isobaric: IsobaricData,
+    val distance: Distance,
+    val bearing: Direction,
+)
