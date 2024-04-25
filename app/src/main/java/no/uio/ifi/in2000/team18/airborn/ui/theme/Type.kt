6f6426f0788c6d9802bcc3841f6761ec38218da2
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-25 18:19:30 +0200

    created new fontfamily

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/theme/Type.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/theme/Type.kt
@@ -2,14 +2,25 @@ package no.uio.ifi.in2000.team18.airborn.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import no.uio.ifi.in2000.team18.airborn.R
+
+val airbornFonts = listOf(
+    Font(R.font.montserratregular),
+    Font(R.font.montserratblack),
+    Font(R.font.montserratbold),
+    Font(R.font.montserratmedium),
+    Font(R.font.montserratextrabold),
+)
+val airbornFontFamily = FontFamily(airbornFonts)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = airbornFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
