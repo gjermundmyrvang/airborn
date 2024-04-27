4eacd2f8d44c98b443fe130d09fd868f3373c468
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-27 15:15:34 +0200

    change nearbywebcamsection from using card to a row with a box indicator when selected

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/Webcam.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/webcam/Webcam.kt
@@ -1,12 +1,10 @@
 package no.uio.ifi.in2000.team18.airborn.ui.webcam
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -18,10 +16,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.SpanStyle
@@ -104,42 +102,29 @@ fun NearbyWebcam(
     webcam: Webcam, current: Webcam, onWebcamClicked: (Webcam) -> Unit
 ) {
     val borderColor =
-        if (webcam == current) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.outline
-    OutlinedCard(
+        if (webcam == current) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.outline
+    Row(
         modifier = Modifier
-            .height(IntrinsicSize.Min)
-            .padding(2.dp)
+            .height(50.dp)
             .fillMaxWidth()
+            .padding(start = 5.dp, end = 5.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .background(MaterialTheme.colorScheme.tertiaryContainer, RoundedCornerShape(5.dp))
             .clickable { onWebcamClicked(webcam) },
-        border = BorderStroke(1.dp, color = borderColor.copy(alpha = 0.5f)),
-        colors = CardDefaults.cardColors(containerColor = Color.Transparent)
     ) {
-        Row(
-            modifier = Modifier.padding(0.dp), verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                Modifier
-                    .fillMaxHeight()
-                    .width(5.dp)
-                    .background(borderColor)
-            )
-            Row(
-                modifier = Modifier.padding(8.dp)
-            ) {
-                AsyncImage(
-                    model = webcam.images.current.thumbnail,
-                    contentDescription = webcam.title,
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Column {
-                    Text(text = webcam.title, fontWeight = FontWeight.Bold)
-                    Text(
-                        text = "updated: ${webcam.lastUpdatedOn.dayNumberMonthTime} (LT)",
-                        fontSize = 15.sp
-                    )
-                }
-            }
-        }
+        Box(
+            Modifier
+                .fillMaxHeight()
+                .width(5.dp)
+                .background(borderColor)
+        )
+        AsyncImage(
+            model = webcam.images.current.thumbnail,
+            modifier = Modifier.fillMaxHeight(),
+            contentDescription = webcam.title,
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = webcam.title, fontWeight = FontWeight.Bold, fontSize = 12.sp)
     }
 }
 
