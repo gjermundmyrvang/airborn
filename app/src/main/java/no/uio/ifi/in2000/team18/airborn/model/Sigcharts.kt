3b10e65cf64c9949ba2c239d7ce674e5fc866d4b
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-27 20:48:35 +0200

    change sigchart dataclass time value to a datetime object

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/model/Sigcharts.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/model/Sigcharts.kt
@@ -1,11 +1,16 @@
 package no.uio.ifi.in2000.team18.airborn.model
 
+import com.google.gson.annotations.JsonAdapter
+import no.uio.ifi.in2000.team18.airborn.ui.common.DateTime
+import no.uio.ifi.in2000.team18.airborn.ui.common.DateTimeAdapter
+
 data class Sigchart(
     val params: SigchartParameters, val updated: String, val uri: String
 )
 
 data class SigchartParameters(
-    val area: Area, val time: String
+    val area: Area,
+    @JsonAdapter(DateTimeAdapter::class) val time: DateTime
 )
 
 enum class Area {
