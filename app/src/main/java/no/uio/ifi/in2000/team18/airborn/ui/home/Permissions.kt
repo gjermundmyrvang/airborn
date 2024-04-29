0d9a68ca1b98440a25a4b75a0bb6a4d74557a668
otberger <otberger@uio.no>
2024-04-29 13:39:32 +0200

    Minor design alterations

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/Permissions.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/Permissions.kt
@@ -63,8 +63,6 @@ fun Content(showButton: Boolean = true, onClick: () -> Unit) {
         val enableLocation = remember { mutableStateOf(true) }
         if (enableLocation.value) {
             CustomDialogLocation(
-                title = "Turn On Location Service",
-                desc = "Explore the world without getting lost and keep the track of your location.\n\nGive this app a permission to proceed. If it doesn't work, then you'll have to do it manually from the settings.",
                 enableLocation,
                 onClick
             )
