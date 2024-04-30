90e87a20afb5fc7ff547877fb7e1153b65e645e6
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-04-30 23:13:27 +0200

    use mutex in sigmetrepository for thread safety

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/SigmetRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/data/repository/SigmetRepository.kt
@@ -1,13 +1,20 @@
 package no.uio.ifi.in2000.team18.airborn.data.repository
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import no.uio.ifi.in2000.team18.airborn.data.datasource.SigmetDatasource
 import no.uio.ifi.in2000.team18.airborn.data.repository.parsers.parseSigmets
 import no.uio.ifi.in2000.team18.airborn.model.Sigmet
 import javax.inject.Inject
 
 class SigmetRepository @Inject constructor(private val dataSource: SigmetDatasource) {
-    private var sigmetDataCache: List<Sigmet>? = null
-    suspend fun fetchSigmets() = sigmetDataCache ?: parseSigmets(dataSource.fetchSigmets()).also {
-        sigmetDataCache = it
+    private val sigmetMutex = Mutex()
+    private var sigmetDataCache: List<Sigmet> = listOf()
+    suspend fun fetchSigmets(): List<Sigmet> {
+        if (sigmetDataCache.isEmpty()) {
+            val sigmets = parseSigmets(dataSource.fetchSigmets())
+            sigmetMutex.withLock { this.sigmetDataCache = sigmets }
+        }
+        return sigmetMutex.withLock { this.sigmetDataCache }
     }
 }
