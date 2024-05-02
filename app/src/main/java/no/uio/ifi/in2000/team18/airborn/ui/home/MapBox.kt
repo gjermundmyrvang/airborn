b9db915397fd8cc5e26dc59afa4d83cb1a94ff9e
Gjermund Persson Myrvang <gjermupm@ulrik.uio.no>
2024-05-02 17:46:55 +0200

    rename function to start with lowercase letter

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/MapBox.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/home/MapBox.kt
@@ -594,7 +594,7 @@ fun AlertLocationPermissionDialog(onDismiss: (Boolean) -> Unit) {
         confirmButton = {
             Button(
                 onClick = {
-                    OpenSettings(context)
+                    openSettings(context)
                 },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -611,12 +611,13 @@ fun AlertLocationPermissionDialog(onDismiss: (Boolean) -> Unit) {
     )
 }
 
-fun OpenSettings(context: Context) {
+fun openSettings(context: Context) {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
     val uri = Uri.fromParts("package", context.packageName, null)
     intent.data = uri
     context.startActivity(intent)
 }
+
 @Preview(showSystemUi = true)
 @Composable
 fun TestInfoBox() {
