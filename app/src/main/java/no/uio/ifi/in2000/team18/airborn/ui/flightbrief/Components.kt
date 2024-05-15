d764f071f86ff1a0fe4c9954a00e13ec0677c1ab
Gjermund Persson Myrvang <gjermupm@uio.no>
2024-05-15 16:26:28 +0200

    delete unused extension function

--- app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/Components.kt
+++ app/src/main/java/no/uio/ifi/in2000/team18/airborn/ui/flightbrief/Components.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.in2000.team18.airborn.ui.flightbrief
 
-import android.graphics.BlurMaskFilter
 import android.os.Build.VERSION.SDK_INT
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
@@ -51,13 +50,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -132,36 +127,6 @@ fun LoadingScreen() {
         )
     }
 }
-
-fun Modifier.shadow(
-    color: Color = Color.Black,
-    offsetX: Dp = 0.dp,
-    offsetY: Dp = 0.dp,
-    blurRadius: Dp = 0.dp,
-) = then(drawBehind {
-    drawIntoCanvas { canvas ->
-        val paint = Paint()
-        val frameworkPaint = paint.asFrameworkPaint()
-        if (blurRadius != 0.dp) {
-            frameworkPaint.maskFilter =
-                (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
-        }
-        frameworkPaint.color = color.toArgb()
-
-        val leftPixel = offsetX.toPx()
-        val topPixel = offsetY.toPx()
-        val rightPixel = size.width + topPixel
-        val bottomPixel = size.height + leftPixel
-
-        canvas.drawRect(
-            left = leftPixel,
-            top = topPixel,
-            right = rightPixel,
-            bottom = bottomPixel,
-            paint = paint,
-        )
-    }
-})
 
 @Composable
 fun MultiToggleButton(
